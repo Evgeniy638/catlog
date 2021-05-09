7a6f92c84db00c25baaa58bc5184f4bfdf6c5a10
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2021-05-09 16:39:12 +0300

    changed security config

--- src/main/java/com/copy/reddit/config/WebSecurityConfig.java
+++ src/main/java/com/copy/reddit/config/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/clients", "posts/likes/**", "posts/comments/create_comment", "posts/comments/create_answer").authenticated()
+                .antMatchers("posts/likes/**", "/posts").authenticated()
                 .and()
                 .httpBasic()
                 .and()
