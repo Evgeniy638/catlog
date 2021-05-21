463bb0aadc55114fbaf587fbc46df90301a6e197
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2021-05-21 22:52:25 +0300

    added ConfigurationPropertiesScan

--- src/main/java/com/copy/reddit/RedditApplication.java
+++ src/main/java/com/copy/reddit/RedditApplication.java
@@ -2,8 +2,10 @@ package com.copy.reddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan(value = "com.copy.reddit.config")
 public class RedditApplication {
 
     public static void main(String[] args) {
